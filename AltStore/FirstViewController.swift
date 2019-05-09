979eda65e176633d73e7c43e3cadf5559ce960b1
Riley Testut <riley@rileytestut.com>
2019-05-09 12:45:30 -0700

    Initial Commit

---
+++ AltStore/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  AltStore
+//
+//  Created by Riley Testut on 5/9/19.
+//  Copyright © 2019 Riley Testut. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
