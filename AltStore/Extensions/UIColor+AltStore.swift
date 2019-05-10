ee79230d3700343bb656f1e348e8a224cdf9d188
Riley Testut <riley@rileytestut.com>
2019-05-10 11:39:34 -0700

    Changes tint color to Delta purple

---
+++ AltStore/Extensions/UIColor+AltStore.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor+AltStore.swift
+//  AltStore
+//
+//  Created by Riley Testut on 5/9/19.
+//  Copyright © 2019 Riley Testut. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor
+{
+    static let altPurple = UIColor(named: "Purple")!
+}
