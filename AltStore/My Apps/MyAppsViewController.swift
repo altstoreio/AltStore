3154a86b7a87c6efd83b62587fb373038bf5aa9e
Riley Testut <riley@rileytestut.com>
2019-05-20 21:36:39 +0200

    Adds support for “deleting” installed apps
    Temporary, because later we won’t be able to actually uninstall apps from within AltStore

--- AltStore/My Apps/MyAppsViewController.swift
+++ AltStore/My Apps/MyAppsViewController.swift
@@ -38,6 +38,7 @@ private extension MyAppsViewController
         fetchRequest.returnsObjectsAsFaults = false
         
         let dataSource = RSTFetchedResultsTableViewDataSource(fetchRequest: fetchRequest, managedObjectContext: DatabaseManager.shared.viewContext)
+        dataSource.proxy = self
         dataSource.cellConfigurationHandler = { (cell, installedApp, indexPath) in
             guard let app = installedApp.app else { return }
             
@@ -54,5 +55,29 @@ private extension MyAppsViewController
         }
         
         return dataSource
+    }
+}
+
+extension MyAppsViewController
+{
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath)
+    {
+        guard editingStyle == .delete else { return }
+        
+        let installedApp = self.dataSource.item(at: indexPath)
+        
+        DatabaseManager.shared.persistentContainer.performBackgroundTask { (context) in
+            let installedApp = context.object(with: installedApp.objectID) as! InstalledApp
+            context.delete(installedApp)
+            
+            do
+            {
+                try context.save()
+            }
+            catch
+            {
+                print("Failed to delete installed app.", error)
+            }
+        }
     }
 }
