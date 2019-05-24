8abf519c94b688ab2b6d00a2d4b38d294a93478a
Riley Testut <riley@rileytestut.com>
2019-05-24 11:29:27 -0700

    Adds AltServer macOS target

---
+++ AltServer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AltServer
+//
+//  Created by Riley Testut on 5/24/19.
+//  Copyright © 2019 Riley Testut. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AltServer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AltServer
+//
+//  Created by Riley Testut on 5/24/19.
+//  Copyright © 2019 Riley Testut. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
