2947b5c9921ef6aa45967da7c97edbbe84379d54
Riley Testut <riley@rileytestut.com>
2019-06-06 12:46:08 -0700

    [AltKit] Adds Result.value and Result.error convenience properties

--- AltKit/Result+Conveniences.swift
+++ AltKit/Result+Conveniences.swift
@@ -10,6 +10,22 @@ import Foundation
 
 public extension Result
 {
+    var value: Success? {
+        switch self
+        {
+        case .success(let value): return value
+        case .failure: return nil
+        }
+    }
+    
+    var error: Failure? {
+        switch self
+        {
+        case .success: return nil
+        case .failure(let error): return error
+        }
+    }
+    
     init(_ value: Success?, _ error: Failure?)
     {
         switch (value, error)
