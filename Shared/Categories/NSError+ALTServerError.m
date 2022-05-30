2cbebbe9b785a56bd5c4eb39cbd14e707afcaab3
Joe Mattiello <mail@joemattiello.com>
2022-05-30 12:50:03 -0400

    WiFi to Wi-Fi spelling (#6)
    Signed-off-by: Joseph Mattello <mail@joemattiello.com>

--- Shared/Categories/NSError+ALTServerError.m
+++ Shared/Categories/NSError+ALTServerError.m
@@ -153,7 +153,7 @@ NSErrorUserInfoKey const ALTOperatingSystemVersionErrorKey = @"ALTOperatingSyste
     {
         case ALTServerErrorConnectionFailed:
         case ALTServerErrorDeviceNotFound:
-            return NSLocalizedString(@"Make sure you have trusted this device with your computer and WiFi sync is enabled.", @"");
+            return NSLocalizedString(@"Make sure you have trusted this device with your computer and Wi-Fi sync is enabled.", @"");
             
         case ALTServerErrorPluginNotFound:
             return NSLocalizedString(@"Make sure Mail is running and the plug-in is enabled in Mail's preferences.", @"");
