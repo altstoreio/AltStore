33674397b151119239ea70f2d6071b151b0167b9
Riley Testut <riley@rileytestut.com>
2022-09-12 15:42:33 -0700

    Adds AppVersion Core Data entity
    Preserves redundant fields on StoreApp in database model for backwards compatibility.

---
+++ AltStoreCore/Model/AppVersion.swift
@@ -0,0 +1,57 @@
+//
+//  AppVersion.swift
+//  AltStoreCore
+//
+//  Created by Riley Testut on 8/18/22.
+//  Copyright © 2022 Riley Testut. All rights reserved.
+//
+
+import CoreData
+
+@objc(AppVersion)
+public class AppVersion: NSManagedObject, Decodable, Fetchable
+{
+    /* Properties */
+    @NSManaged public var version: String
+    @NSManaged public var date: Date
+    @NSManaged public var localizedDescription: String?
+    
+    @NSManaged public var downloadURL: URL
+    @NSManaged public var size: Int64
+    
+    @nonobjc public var minOSVersion: OperatingSystemVersion? {
+        guard let osVersionString = self._minOSVersion else { return nil }
+        
+        let osVersion = OperatingSystemVersion(string: osVersionString)
+        return osVersion
+    }
+    @NSManaged @objc(minOSVersion) private var _minOSVersion: String?
+    
+    @nonobjc public var maxOSVersion: OperatingSystemVersion? {
+        guard let osVersionString = self._maxOSVersion else { return nil }
+        
+        let osVersion = OperatingSystemVersion(string: osVersionString)
+        return osVersion
+    }
+    @NSManaged @objc(maxOSVersion) private var _maxOSVersion: String?
+    
+    @NSManaged public var appBundleID: String
+    @NSManaged public var sourceID: String?
+    
+    /* Relationships */
+    @NSManaged public private(set) var app: StoreApp?
+    @NSManaged public private(set) var latestVersionApp: StoreApp?
+    
+    private override init(entity: NSEntityDescription, insertInto context: NSManagedObjectContext?)
+    {
+        super.init(entity: entity, insertInto: context)
+    }
+}
+
+public extension AppVersion
+{
+    @nonobjc class func fetchRequest() -> NSFetchRequest<AppVersion>
+    {
+        return NSFetchRequest<AppVersion>(entityName: "AppVersion")
+    }
+}
