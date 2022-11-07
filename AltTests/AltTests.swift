cb0e3d5df0306503786dd3b01eceb87b1a6a63fd
Riley Testut <riley@rileytestut.com>
2022-11-07 16:46:32 -0600

    Adds AltTests test target

---
+++ AltTests/AltTests.swift
@@ -0,0 +1,22 @@
+//
+//  AltTests.swift
+//  AltTests
+//
+//  Created by Riley Testut on 10/6/22.
+//  Copyright © 2022 Riley Testut. All rights reserved.
+//
+
+import XCTest
+
+final class AltTests: XCTestCase
+{
+    override func setUpWithError() throws
+    {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws
+    {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+}
---
+++ AltTests/TestErrors.swift
@@ -0,0 +1,7 @@
+//
+//  TestErrors.swift
+//  AltTests
+//
+//  Created by Riley Testut on 10/17/22.
+//  Copyright © 2022 Riley Testut. All rights reserved.
+//
