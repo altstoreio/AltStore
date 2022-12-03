4a5ca81e9aa3ba446fae1d71ce62c389c33f5894
Nythepegasus <nythepegasus84@gmail.com>
2022-12-03 17:24:07 -0500

    Show file extensions to help user choose file

--- AltStore/LaunchViewController.swift
+++ AltStore/LaunchViewController.swift
@@ -86,6 +86,7 @@ class LaunchViewController: RSTLaunchViewController, UIDocumentPickerDelegate
                 types.append(contentsOf: UTType.types(tag: "mobiledevicepairing", tagClass: UTTagClass.filenameExtension, conformingTo: UTType.data))
                 types.append(.xml)
                 let documentPickerController = UIDocumentPickerViewController(forOpeningContentTypes: types)
+                documentPickerController.shouldShowFileExtensions = true
                 documentPickerController.delegate = self
                 self.present(documentPickerController, animated: true, completion: nil)
              })
