742b51e5e2d354417c0db6efa1ef808f1146d1d8
Joseph Mattello <mail@joemattiello.com>
2022-12-17 03:12:13 -0500

    fix deprecated style .white to .medium
    Signed-off-by: Joseph Mattello <mail@joemattiello.com>

--- AltStore/Settings/PatreonComponents.swift
+++ AltStore/Settings/PatreonComponents.swift
@@ -40,7 +40,7 @@ class PatronsFooterView: UICollectionReusableView
         super.init(frame: frame)
         
         self.button.translatesAutoresizingMaskIntoConstraints = false
-        self.button.activityIndicatorView.style = .white
+        self.button.activityIndicatorView.style = .medium
         self.button.titleLabel?.textColor = .white
         self.addSubview(self.button)
         
