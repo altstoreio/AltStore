515ba0f4e1467e9b5fe5e38c6602ac170f0636cd
Riley Testut <riley@rileytestut.com>
2023-03-02 15:00:54 -0600

    Fixes UIApplication.keyWindow deprecation warning

--- AltStoreCore/Patreon/PatreonAPI.swift
+++ AltStoreCore/Patreon/PatreonAPI.swift
@@ -417,6 +417,21 @@ extension PatreonAPI: ASWebAuthenticationPresentationContextProviding
 {
     public func presentationAnchor(for session: ASWebAuthenticationSession) -> ASPresentationAnchor
     {
-        return UIApplication.alt_shared?.keyWindow ?? UIWindow()
+        //TODO: Properly support multiple scenes.
+        
+        guard let windowScene = UIApplication.alt_shared?.connectedScenes.lazy.compactMap({ $0 as? UIWindowScene }).first else { return UIWindow() }
+
+        if #available(iOS 15, *), let keyWindow = windowScene.keyWindow
+        {
+            return keyWindow
+        }
+        else if let delegate = windowScene.delegate as? UIWindowSceneDelegate,
+                let optionalWindow = delegate.window,
+                let window = optionalWindow
+        {
+            return window
+        }
+
+        return UIWindow()
     }
 }
