1f499e77d3a88b49a5fb0e65dfa7dee0806ffcee
Riley Testut <riley@rileytestut.com>
2023-09-08 13:47:07 -0500

    [AltJIT] Removes unnecessary ALTErrorKeys.h/.m
    Was originally added because AltJIT couldn’t link with AltSign, which is not true anymore.

--- AltJIT/AltJIT-Bridging-Header.h
+++ AltJIT/AltJIT-Bridging-Header.h
@@ -2,8 +2,6 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import "ALTErrorKeys.h"
-
 // Shared
 #import "ALTWrappedError.h"
 #import "NSError+ALTServerError.h"
