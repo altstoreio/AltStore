a8589526f15a844c1c19b81a0f2cbe245de41aff
Riley Testut <riley@rileytestut.com>
2023-12-07 17:02:13 -0600

    Fixes accidentally saving CancellationErrors to error log

--- AltStore/Managing Apps/AppManager.swift
+++ AltStore/Managing Apps/AppManager.swift
@@ -1914,6 +1914,7 @@ private extension AppManager
         switch error
         {
         case is CancellationError: return // Don't log CancellationErrors
+        case let nsError as NSError where nsError.domain == CancellationError()._domain: return
         default: break
         }
         
